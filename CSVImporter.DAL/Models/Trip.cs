01f48a47e6663b3d3bf48dc6e82fb18ae38980b7
f1atteee <nazarrkravets@gmail.com>
2025-10-25 23:44:58 +0300

    code review

--- CSVImporter.DAL/Models/Trip.cs
+++ CSVImporter.DAL/Models/Trip.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSVImporter.DAL.Models
+﻿namespace CSVImporter.DAL.Models
 {
     public class Trip
     {
